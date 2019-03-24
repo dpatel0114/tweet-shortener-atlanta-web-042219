957e756907ca13b8e4aaca512fe2f83087b5d378
Dhara Patel <1991dharapatel@gmail.com>
2019-03-24 16:00:01 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -45,7 +45,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  if selective_tweet_shortener >140
+  if selective_tweet_shortener(tweet).length >140
     word_substituter(tweet)
   else
     tweet
