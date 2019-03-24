17c6f174c0eeee1bfbacd25f24dcbf43401b2560
Dhara Patel <1991dharapatel@gmail.com>
2019-03-24 15:56:45 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,59 @@
 # Write your code here.
+require 'pry'
+
+def dictionary
+word2sub= {
+"hello" => 'hi',
+"to" => '2' ,
+"two" => '2',
+"too" => '2',
+"for" => '4',
+"four" =>'4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@" ,
+"and" => "&"
+}
+end
+
+def word_substituter(tweet)
+  my_tweet = tweet.split(" ")
+  result = my_tweet.map do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else 
+      word
+      end 
+  end 
+    result.join(" ")
+end
+
+def bulk_tweet_shortener(tweet_array)
+  # binding.pry 
+
+for element in tweet_array
+puts word_substituter(element)
+end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+     word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if selective_tweet_shortener >140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+
+
+
+
+  
